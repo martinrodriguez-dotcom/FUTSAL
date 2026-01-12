ea5463ad4d887e0162d467d4a82cde983ba94ebc
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2026-01-12 14:47:58 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'futsal-app-cache-v5.6'; // Subimos versión para forzar actualización
+const CACHE_NAME = 'futsal-app-cache-v5.7'; // Subimos versión para forzar actualización
 const urlsToCache = [
     '/',
     '/index.html',
